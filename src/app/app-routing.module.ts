c11eb2303889ba84808c07860d81f9e9bf4d9372
Vijay <vijayv2694@gmail.com>
2021-08-21 00:22:05 +0530

    Add AngularFireAuthGuard redirect to Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,15 +10,19 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { AuthGuard } from './guards/auth.guard';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
 
 const routes: Routes = [
-  {path:'', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path:'', component: DashboardComponent, canActivate:[AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
-  {path:'client/add', component: AddClientComponent, canActivate:[AuthGuard]},
-  {path:'client/edit/:id', component: EditClientComponent, canActivate:[AuthGuard]},
-  {path:'client/:id', component: ClientDetailsComponent, canActivate:[AuthGuard]},
-  {path:'settings', component: SettingsComponent, canActivate:[AuthGuard]},
+  {path:'client/add', component: AddClientComponent, canActivate:[AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  {path:'client/edit/:id', component: EditClientComponent, canActivate:[AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  {path:'client/:id', component: ClientDetailsComponent, canActivate:[AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
+  {path:'settings', component: SettingsComponent, canActivate:[AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin }},
   // {path:'clients', component: ClientsComponent},
   {path:'**', component: NotFoundComponent}
 ];
